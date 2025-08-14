2d6b0c1cedd330264160c36270976ec837df16b5
Chainingrobin <youssefyd04@gmail.com>
2025-08-14 21:07:47 +0300

    callback fix blank page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+// src/App.tsx
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { handleSpotifyCallback } from "./auth/spotifyAuth";
 import { TarotLayout } from "components/Tarotcards/TarotCards";
 import Background from "./components/Background/Background";
 import { p5logopic } from "assets";
-import clickSound from "assets/fr.mp3"; // Change filename to your actual mp3
+import clickSound from "assets/fr.mp3";
 import "./App.css";
 import callingcard from "assets/images/imagetexts/callingcard.png";
 
-// This component only runs when Spotify redirects back to /callback
 function Callback() {
   useEffect(() => {
     handleSpotifyCallback().then((token) => {
       console.log("Access token:", token);
-      // Redirect back to main page
+      // After finishing the login, go back to the main page
       window.location.href = "/";
     });
   }, []);
@@ -21,8 +21,7 @@ function Callback() {
   return <p>Completing Spotify login...</p>;
 }
 
-// Your main tarot page layout moved into its own component
-function HomePage() {
+function Home() {
   const [showModal, setShowModal] = useState(true);
 
   const playSound = () => {
@@ -70,8 +69,8 @@ function HomePage() {
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />        {/* main tarot UI */}
-      <Route path="/callback" element={<Callback />} /> {/* Spotify redirect */}
+      <Route path="/" element={<Home />} />
+      <Route path="/callback" element={<Callback />} />
     </Routes>
   );
 }
