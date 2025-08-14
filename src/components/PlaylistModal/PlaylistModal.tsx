b35c57c5095658400ff680ec13954b29695d8342
Chainingrobin <youssefyd04@gmail.com>
2025-08-14 20:22:25 +0300

    vercel capital

--- src/components/PlaylistModal/PlaylistModal.tsx
+++ src/components/PlaylistModal/PlaylistModal.tsx
@@ -1,7 +1,7 @@
 /// <reference types="spotify-web-playback-sdk" />
 
 import React, { useEffect, useState } from "react";
-import "./playlistModal.css";
+import "./PlaylistModal.css";
 
 // ---- GLOBAL DECLARATIONS FOR SPOTIFY SDK ----
 declare global {
