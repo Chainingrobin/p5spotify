c2c6ae4470b2488070b0a4f7a6ea9c6c70c37993
Chainingrobin <youssefyd04@gmail.com>
2025-08-14 20:09:03 +0300

    vercel slimming

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,8 @@
-export async function getPlaylist(playlistId: string) {
-  const res = await fetch(`/api/playlist?id=${encodeURIComponent(playlistId)}`);
-  
+export async function getPlaylist(playlistId: string, accessToken: string) {
+  const res = await fetch(`/api/playlist/${encodeURIComponent(playlistId)}`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
   if (!res.ok) throw new Error(`Error fetching playlist: ${res.statusText}`);
   return res.json();
 }
