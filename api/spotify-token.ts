80e9a3e52d3e2fc3b1b45e010f25357d321eae04
Chainingrobin <youssefyd04@gmail.com>
2025-08-15 22:06:15 +0300

    spotify token shit again

--- api/spotify-token.ts
+++ api/spotify-token.ts
@@ -1,6 +1,5 @@
 // api/spotify-token.ts
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import fetch from 'node-fetch';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
