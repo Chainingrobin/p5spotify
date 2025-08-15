fb67a011e115218d8ec3079a83bb3c9e62d4e03e
Chainingrobin <youssefyd04@gmail.com>
2025-08-15 12:23:29 +0300

    callback

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -10,7 +10,6 @@ export default function Callback() {
       const token = await handleSpotifyCallback();
       if (token) {
         console.log("Spotify access token stored:", token);
-        navigate("/"); // After login, send back to home page (or another page)
       } else {
         console.error("Spotify login failed.");
         navigate("/"); // Could send to an error page if you prefer
